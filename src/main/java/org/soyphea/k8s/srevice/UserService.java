33b46ba31fdc89d82e9f5ae21a11315031df855b
rmedinaf <rmedinaf.everis@bancochile.cl>
2021-06-19 11:59:46 -0400

    se crea clase Password

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -1,5 +1,6 @@
 package org.soyphea.k8s.srevice;
 
+import org.soyphea.k8s.domain.Password;
 import org.soyphea.k8s.domain.User;
 import org.springframework.stereotype.Service;
 import java.util.Arrays;
@@ -12,7 +13,8 @@ public class UserService {
     List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
 
     public List<User> getUser(String containName) {
-
+        Password password = new Password();
+        password.setClave("123124124");
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
     }
 
